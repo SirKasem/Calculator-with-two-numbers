ee174cce2ade6096b2353249fd3faa0474d2b3fb
Kasem1509 <iamkasem88@gmail.com>
2025-08-15 21:03:00 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,33 @@
+
+function calculate(){
+
+    let num1 = parseInt(document.getElementById("num1").value);
+    let num2 = parseInt(document.getElementById("num2").value);
+    let numcalculate = document.getElementById("Calculate").value;
+     result;
+
+    if (numcalculate === "+"){
+        result = num1 + num2;
+        document.getElementById("result").innerHTML = "The result is : "
+        + result.toString();
+    }
+    else if(numcalculate === "-")
+    {
+        result = num1 - num2;
+        document.getElementById("result").innerHTML = "The result is : "
+        + result.toString();
+    }
+     else if(numcalculate === "*")
+    {
+        result = num1 * num2;
+        document.getElementById("result").innerHTML = "The result is : "
+        + result.toString();
+    }
+     else if(numcalculate === "/")
+    {
+        result = num1 / num2;
+        document.getElementById("result").innerHTML = "The result is : "
+        + result.toString();
+    }
+
+}
