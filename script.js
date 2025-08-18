b80aea4d87abebd84c07acb7e19c3336b9a7ad81
Kasem1509 <iamkasem88@gmail.com>
2025-08-18 13:23:32 +0200

    Javascript file edited

--- script.js
+++ script.js
@@ -1,32 +1,32 @@
+function calculate() {
+  let num1 = parseFloat(document.getElementById("num1").value);
+  let num2 = parseFloat(document.getElementById("num2").value);
+  let numcalculate = document.getElementById("Calculate").value;
 
-function calculate(){
+  // Here i'm valdiating the input from te user...
+  if (isNaN(num1) || isNaN(num2) || num1 === "" || num2 === "") {
+    document.getElementById("result").innerHTML =
+      "Entered input is not correct";
+  }
 
-    let num1 = parseInt(document.getElementById("num1").value);
-    let num2 = parseInt(document.getElementById("num2").value);
-    let numcalculate = document.getElementById("Calculate").value;
-
-    if (numcalculate === "+"){
-        result = num1 + num2;
-        document.getElementById("result").innerHTML = "The result is : "
-        + result.toString();
-    }
-    else if(numcalculate === "-")
-    {
-        result = num1 - num2;
-        document.getElementById("result").innerHTML = "The result is : "
-        + result.toString();
-    }
-     else if(numcalculate === "*")
-    {
-        result = num1 * num2;
-        document.getElementById("result").innerHTML = "The result is : "
-        + result.toString();
-    }
-     else if(numcalculate === "/")
-    {
-        result = num1 / num2;
-        document.getElementById("result").innerHTML = "The result is : "
-        + result.toString();
-    }
-
+  //Here the Calculate funcation
+  else if (numcalculate === "+") {
+    result = num1 + num2;
+    document.getElementById("result").innerHTML =
+      "The result is : " + result.toString();
+  } else if (numcalculate === "-") {
+    result = num1 - num2;
+    document.getElementById("result").innerHTML =
+      "The result is : " + result.toString();
+  } else if (numcalculate === "*") {
+    result = num1 * num2;
+    document.getElementById("result").innerHTML =
+      "The result is : " + result.toString();
+  } else if (numcalculate === "/") {
+    result = num1 / num2;
+    document.getElementById("result").innerHTML =
+      "The result is : " + result.toString();
+  } else {
+    console.log("ERROR should never reach here");
+  }
 }
